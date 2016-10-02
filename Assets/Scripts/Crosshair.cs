e726e27c4587e410442d9c9d9d8d305c4d02e6c6
mengguoru <mengguoru0@gmail.com>
2016-10-02 14:42:57 +0800

    muzzle and bullet out effect

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+    public Texture2D crossHair;
+
+    void OnGUI()
+    {
+        float w = crossHair.width / 2;
+        float h = crossHair.height / 2;
+
+        Rect position = new Rect((Screen.width - w) / 2, (Screen.height - h) / 2, w, h);
+        GUI.DrawTexture(position,crossHair);
+    }
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController: MonoBehaviour {
+
+    public ParticleSystem muzzleFlash;
+    public ParticleSystem bulletOutFlash;
+    public Animator anim;
+    public GameObject impactPrefab;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponentInChildren<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Fire1"))
+        {
+            muzzleFlash.Play();
+            bulletOutFlash.Play();
+            //anim.SetTrigger("Fire");
+        }
+	}
+}
