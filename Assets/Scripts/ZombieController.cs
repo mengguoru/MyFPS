cc21078d887610002d708fa96b3a8f38ac266a1a
mengguoru <mengguoru0@gmail.com>
2016-10-02 23:59:08 +0800

    simple zombie ai

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+
+    public Transform target;
+    public float safetyDis;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(transform.forward);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(safetyDis >= Vector3.Distance(transform.position,target.position))
+        {
+            //Debug.Log("danger");
+            Vector3 tmp = target.position - transform.position;
+            transform.forward = tmp;
+        }
+
+        //Vector3 targetDir = target.position - transform.position;
+        //float angle = Vector3.Angle(targetDir, transform.forward);
+        //Debug.Log(angle);
+    }
+}
