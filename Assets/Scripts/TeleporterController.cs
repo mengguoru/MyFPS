f62de6b089d8c16b7a07c62f00c838d4507ee8ab
mengguoru <mengguoru0@gmail.com>
2016-10-06 15:17:11 +0800

    add teleporter

---
+++ Assets/Scripts/TeleporterController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TeleporterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.transform.tag);
+        if ("Player" == other.transform.tag)
+        {
+            //Debug.Log("load scene");
+            SceneManager.LoadScene("InBigHouse");
+        }
+    }
+}
