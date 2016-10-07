40908bb575018d4eee833fc544361840b4b520fa
mengguoru <mengguoru0@gmail.com>
2016-10-07 22:17:37 +0800

    scene2 add telepoter to end

---
+++ Assets/Scripts/Scene2Telepoter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Scene2Telepoter : MonoBehaviour {
+
+	//// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.transform.tag);
+        if ("Player" == other.transform.tag)
+        {
+            //Debug.Log("load scene");
+            SceneManager.LoadScene("theEnd");
+        }
+    }
+}
