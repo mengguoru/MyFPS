c39c6bc4700f842a405b45213cbceadb531e355c
mengguoru <mengguoru0@gmail.com>
2016-10-07 13:36:57 +0800

    broken bottle animation

---
+++ Assets/Scripts/KnifeController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnifeController : MonoBehaviour {
+
+    Animator anim;
+    bool shooting;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        shooting = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("Fire");
+            shooting = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (shooting)
+        {
+            shooting = false;
+
+            
+        }
+    }
+}
